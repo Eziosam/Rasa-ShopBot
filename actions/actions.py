844e6f58a8fa65ca1de0f8c74e348f22e1a58465
Samir <acrobaticsam@gmail.com>
2021-11-15 21:00:15 +0600

    mega update

--- actions/actions.py
+++ actions/actions.py
@@ -27,7 +27,7 @@ path_to_db = "actions/example.db"
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
-path_to_db = "actions/example.db"
+path_to_db = "actions/db.sqlite3"
 class ActionProductSearch(Action):
 
     def name(self) -> Text:
@@ -40,15 +40,17 @@ class ActionProductSearch(Action):
         connection = sqlite3.connect(path_to_db)
         cursor = connection.cursor()
 
-        shoe= [tracker.latest_message['entities'][0]['value']]
+        pname= [tracker.latest_message['entities'][0]['value']]
 
-        cursor.execute("SELECT * FROM inventory WHERE pname=? ", shoe)
+        cursor.execute("SELECT image FROM inventory WHERE pname=? ", pname)
         data_row = cursor.fetchone()
 
         if data_row:
-          dispatcher.utter_message(text="Found!")
+          dispatcher.utter_message(text="Yes sir we have this product!")
+          dispatcher.utter_message(image=data_row[0]) 
+                        
         else:
-          dispatcher.utter_message(text="Not Available!")  
+          dispatcher.utter_message(text="Sorry , sir your desired product is not currently Available!")  
 
 class ActionProductPriceSearch(Action):
 
@@ -70,7 +72,7 @@ class ActionProductPriceSearch(Action):
         if data_row:
           dispatcher.utter_message("Price of "+ str(pname[0]) +" is "+str(data_row[0]))
         else:
-          dispatcher.utter_message(text="Not Available!")  
+          dispatcher.utter_message(text="Sorry , sir your desired product is not currently Available!")  
 
 class ActionProductQuantitySearch(Action):
 
@@ -115,5 +117,111 @@ class ActionProductDeliveryFeeSearch(Action):
           dispatcher.utter_message("Sir ,The Delivery fee is "+str(data_row[0]) +" USD$ " )
         else:
           dispatcher.utter_message(text="Not Available!") 
+
+
+class ActionProductRangedSearch(Action):
+
+    def name(self) -> Text:
+        return "action_product_in_range"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+
+        category= [tracker.latest_message['entities'][0]['value']]
+        #start=[tracker.latest_message['entities'][1]['value']]
+        #end= [tracker.latest_message['entities'][2]['value']]
+
+        cursor.execute("SELECT pname FROM inventory WHERE category='electronics' and price>=80000 and price <=920000")
+        data_row = cursor.fetchone()
+
+        if data_row:
+          dispatcher.utter_message("Sir ,The the product is "+str(data_row[0]) +" USD$ " )
+        else:
+          dispatcher.utter_message(text="Not Available!") 
+
+
+
+class ActionCarousel(Action):
+    def name(self) -> Text:
+        return "action_carousels"
+    
+    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         
+        
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+
+        pname= ["msi"]
+
+        cursor.execute("SELECT price FROM inventory WHERE rating=5")
+        data_row = cursor.fetchall()
+        
+        
+
+        message = {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "Sony Bravia TV",
+                        "subtitle": data_row[0],
+                        "image_url": "https://www.startech.com.bd/image/cache/catalog/Television/sony/KDL%20W600D-500x500.jpg",
+                        "buttons": [ 
+                            {
+                            "title": "Add to Cart",
+                            "payload": "Happy",
+                            "type": "postback"
+                            },
+                            {
+                            "title": "Show More",
+                            "payload": "sad",
+                            "type": "postback"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Samsung 870 EVO SSD",
+                        "subtitle": "$12",
+                        "image_url": "https://www.startech.com.bd/image/cache/catalog/ssd/samsung/870-evo/870-evo-01-500x500.jpg",
+                        
+                        "buttons": [ 
+                            {
+                            "title": "Add to Cart",
+                            "payload": "Happy",
+                            "type": "postback"
+                            },
+                            {
+                            "title": "Show More",
+                            "payload": "sad",
+                            "type": "postback"
+                            }
+                        ]
+                    },
+                          {
+                        "title": "Core i7 CPU",
+                        "subtitle": "$ "+str(data_row[0]),
+                        "image_url": "https://i.imgur.com/UfDtQuv.jpeg",
+                        "buttons": [ 
+                             {
+                            "title": "Add to Cart",
+                            "payload": "Happy",
+                            "type": "postback"
+                            },
+                            {
+                            "title": "Show More",
+                            "payload": "sad",
+                            "type": "postback"
+                            }
+                            ]
+                        }
+                   ]
+                }
+        }
+        dispatcher.utter_message(attachment=message)
+        return []
 
